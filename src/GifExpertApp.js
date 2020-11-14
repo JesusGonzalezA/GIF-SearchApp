e4cd4abb56c1f3fe32a375ddac71cddde8f24e09
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2020-11-14 13:04:44 +0100

    New changes, it now renders the new component and the list

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -4,20 +4,36 @@
 
 //**************************************************************************
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
+import AddCategory from './components/AddCategory';
 
 //**************************************************************************
 
 const GifExpertApp = () => {
 
+    const [categories, setCategories] = useState([]);
     
-
+    
     return (
-        <header>
-            <h1>GifExpertApp</h1>
-            <hr />
-        </header>
+        <>
+            <header>
+                <h1>GifExpertApp</h1>
+                <hr />
+            </header>
+           
+            <AddCategory setCategories= {setCategories} />
+
+            <div id="list">
+                <ol>
+                    {
+                        categories.map(category => {
+                            return <li key={category}> {category} </li>
+                        })
+                    }
+                </ol>
+            </div>
+            
+        </>
     );
 }
 
