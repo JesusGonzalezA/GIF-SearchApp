d3954333e1fc30b491045dd3f39f6683f9e9f9ec
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2020-12-22 10:43:08 +0100

    Tests upgraded to avoid the hook to unmount

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -1,16 +1,28 @@
+import { waitFor } from "@testing-library/react"
 import { renderHook } from "@testing-library/react-hooks"
 import React from 'react'
 import {useFetchGifs} from "../../hooks/useFetchGifs" 
 
 describe('Hooks -> useFetchGifs', () => {
     
-    test('should return the initial state', () => {
+    test('should return the initial state', async () => {
         
-        const { result } = renderHook( () => useFetchGifs('') );
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('') );
         const { images, loading } = result.current;
+        await waitForNextUpdate();
 
         expect( images ).toStrictEqual( [] );
         expect( loading ).toBe( true )
     })
+
+    test('should return an array and false', async () => {
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('Example') );
+        await waitForNextUpdate();
+        const { images, loading } = result.current;
+
+        expect( images.length ).toBe( 10 );
+        expect( loading ).toBe( false );
+    })
+    
     
 })
